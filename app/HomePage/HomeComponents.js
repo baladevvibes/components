d600c98ceec7c236b9f8d95bd6e338bfc3230583
baladevvibes <balamurugandevvibes@gmail.com>
2024-05-24 06:16:45 +0530

    updates

--- app/HomePage/HomeComponents.js
+++ app/HomePage/HomeComponents.js
@@ -15,13 +15,6 @@ const HomeComponents = [
         tag:"About",
         link:"/about/gallery-frame-aboutus",
     },
-    // {
-    //     _id:"004",
-    //     image:"./Image/about/about001.webp",
-    //     title:"Table with sort",
-    //     tag:"Table",
-    //     link:"/table/table-with-sort/preview",
-    // },
     {
         _id:"001",
         image:"../Image/hero/hero001.webp",
@@ -36,13 +29,7 @@ const HomeComponents = [
         tag:"Card",
         link:"/card/offer-wise-card-section/",
     },
-    // {
-    //     _id:"005",
-    //     image:"./Image/card/card001.webp",
-    //     title:"Offer wise card section",
-    //     tag:"Card",
-    //     link:"/card/three-grid-card-template/preview",
-    // },
+   
 ]
 
 export default HomeComponents
