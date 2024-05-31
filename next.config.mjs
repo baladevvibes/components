a194ffe8cdbc3ba99f616fe01081f12ca904ba84
baladevvibes <balamurugandevvibes@gmail.com>
2024-05-31 20:19:15 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.makecomponents.com" }],
-        destination: "http://makecomponents.com/:path*",
-        permanent: false,
-      },
-    ];
-  },
-  output: "export",
-};
-/* module.exports = nextConfig*/
-export default nextConfig;
+    output:'export',
+  };
+  /* module.exports = nextConfig*/
+  export default nextConfig;
+  
