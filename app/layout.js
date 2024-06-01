1dd1dccc4593ae5e94d67c6021faea962fd201fd
baladevvibes <balamurugandevvibes@gmail.com>
2024-06-01 13:13:14 +0530

    terms

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-adsense-account" content="ca-pub-2500160320143617"></meta>
+
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2500160320143617"
         crossorigin="anonymous"
       ></script>
+      </head>
+   
       <body className={inter.className}>{children}</body>
     </html>
   );
