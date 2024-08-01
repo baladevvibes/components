85a208aa63103334946134ab2f24bb6060faaf69
baladevvibes <balamurugandevvibes@gmail.com>
2024-08-01 08:29:27 +0530

    updates

---
+++ app/product/ProductCard.js
@@ -0,0 +1,74 @@
+"use client"
+import React, { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HomeComponents from "../HomePage/HomeComponents";
+import HomeCard from "../HomePage/HomeCard";
+
+export default function ProductCard() {
+    const [data,setData] = useState(HomeComponents)
+    var keyword= "product"
+
+ 
+    useEffect(()=>{
+        let localData= []
+        HomeComponents?.forEach((el)=>{
+            if(el?.tag===keyword){
+                localData.push(el)
+            }
+
+        })
+        setData(localData)
+    },[])
+  return (
+    <div>
+      <div className={` `}>
+        {/* <DatasetJsonLd
+      description="The description needs to be at least 50 characters long"
+      name="name of the dataset"
+      license="https//www.example.com"
+    /> */}
+        <Header />
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-2500160320143617"
+          data-ad-slot="2477731086"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+
+  
+
+        <div className={`pt-[150px] sm:px-4 container mx-auto py-6`}>
+
+        <h2 class=" title-font pt-16 pb-10 text-center text-secondary text-2xl font-semibold aos-init aos-animate">Ecommerce Product Components</h2>
+          <div
+            className={` grid lg:grid-cols-3 lge:grid-cols-2 md:grid-cols-2 mdsm:grid-cols-2 sm:grid-cols-1 sm:px-2 gap-5`}
+          >
+            {data?.map((v, i) => {
+              return (
+                <HomeCard
+                  title={v?.title}
+                  img={v?.image}
+                  tag={v?.tag}
+                  link={v?.link}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-2500160320143617"
+          data-ad-slot="2477731086"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
--- app/product/page.js
+++ app/product/page.js
@@ -1,7 +1,81 @@
 import React from 'react'
+import ProductCard from './ProductCard'
+
+export const metadata = {
+  keywords:
+    "ecommerce product components, Ecommerce product components examples, Ecommerce product components list",
+  description:
+    "Ecommerce product components list page. It more useful for the users.",
+  icons: {
+    icon: "../favicon.ico",
+    shortcut: "../favicon.ico",
+    apple: "../favicon.ico",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "../favicon.ico",
+    },
+  },
+  openGraph: {
+    title: "Ecommerce Product Components",
+    description:
+      "Ecommerce product components list page. It more useful for the users.",
+    url: "https://makecomponents.com/product",
+    siteName: "makecomponents.com",
+    alternates: {
+      canonical: `https://makecomponents.com/product`,
+    },
+    images: [
+      {
+        url: "https://makecomponents.com/Image/product/product001.webp", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://makecomponents.com/Image/product/product001.webp", // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt: "Product components",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  metadataBase: new URL(`https://makecomponents.com/product`),
+  title: {
+    default: `Ecommerce Product Components`,
+  },
+  alternates: {
+    canonical: "https://makecomponents.com/product",
+  },
+};
 
 export default function page() {
+  const jsonLd = {
+    "@context": "https://schema.org/",
+    "@type": "BreadcrumbList",
+    name: "Ecommerce Product Components",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: "https://makecomponents.com/",
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Ecommerce Product Components",
+        item: "https://makecomponents.com/product",
+      },
+    ],
+  };
   return (
-    <div>page</div>
+    <div>
+          <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <ProductCard/>
+    </div>
   )
 }
