31afa7d959371a13829b9a849b8af42f67908516
baladevvibes <balamurugandevvibes@gmail.com>
2024-10-16 08:16:45 +0530

    updates

--- app/testimonials/testimonials-tailwind/TestimonialsTailwind.js
+++ app/testimonials/testimonials-tailwind/TestimonialsTailwind.js
@@ -653,7 +653,83 @@ function TestimonialsTailwind() {
               ) : (
                 <>
                   <div>
-                    <div className={ ` lg:py-10 lge:py-10 md:py-10 mdsm:py-10 sm:py-10 sm:pb-10  ${
+
+                  <div className="  container mx-auto py-32">
+                    <div className={` ${xlScreen ? `w-[w-full]`:``} ${lgScreen ? `w-[980px]`:``} ${mdScreen ? `w-[780px]`:``} ${smScreen ? `w-[600px]`:``} ${responsiveState ? ``:` sm:w-[600px] md:w-[780px] lg:w-full lge:w-full mdsm:w-[980px]`} mx-auto`}>
+
+          
+                    
+        <div className={` grid ${xlScreen?` grid-cols-2`:``} ${lgScreen?` grid-cols-1`:``} ${mdScreen?` grid-cols-1`:``} ${smScreen?` grid-cols-1`:``} ${responsiveState ? ``:`lg:grid-cols-2 lge:grid-cols-2 md:grid-cols-1 mdsm:grid-cols-1 sm:grid-cols-1`}`}>
+          <div className=" bg-[#3066BE]">
+            <div className=" grid grid-cols-2 sm:grid-cols-1">
+              <div className=" p-6 pt-20">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  class=" text-4xl text-[#FBFF12]"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path>
+                </svg>
+                <p className=" text-[#f2f2f2] pt-4 work leading-8 font-semibold text-[24px] w-[130%] sm:w-full">
+                  Lorem ipsum is placeholder text commonly used in the graphic,
+                  print, and publishing industries for previewing layouts and
+                  visual mockups.
+                </p>
+                <h3 className="pt-4 text-[18px] work text-[#f2f2f2]">
+                  - Sean Roldan
+                </h3>
+              </div>
+              <div className=" w-full relative flex justify-end">
+                <img
+                  src="http://localhost:3000/Image/overall/person/person14.webp"
+                  className=" h-[400px] sm:hidden absolute bottom-0"
+                  alt="Person"
+                />
+              </div>
+            </div>
+          </div>
+          <div className=" bg-[#9368B7]">
+            <div className=" grid grid-cols-2 sm:grid-cols-1">
+              <div className=" p-6 pt-20">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  class=" text-4xl text-[#FBFF12]"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path>
+                </svg>
+                <p className=" text-[#f2f2f2] pt-4 work leading-8 font-semibold text-[24px] w-[130%] sm:w-full">
+                  Lorem ipsum is placeholder text commonly used in the graphic,
+                  print, and publishing industries for previewing layouts and
+                  visual mockups.
+                </p>
+                <h3 className="pt-4 text-[18px] work text-[#f2f2f2]">
+                  - Emma Watson
+                </h3>
+              </div>
+              <div className=" w-full flex justify-end relative">
+                <img
+                  src="http://localhost:3000/Image/overall/person/person13.webp"
+                  className=" h-[400px] sm:hidden absolute bottom-0"
+                  alt="Person"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+                    {/* <div className={ ` lg:py-10 lge:py-10 md:py-10 mdsm:py-10 sm:py-10 sm:pb-10  ${
                           xlScreen ? `h-[100vh]` : ``
                         }  ${lgScreen ? `h-[100vh]` : ``}  ${
                           mdScreen ? `h-[80vh]` : ``
@@ -668,7 +744,7 @@ function TestimonialsTailwind() {
                           mdScreen ? `w-[780px]` : ``
                         } ${smScreen ? `w-[600px]` : ``}`}
                       />
-                    </div>
+                    </div> */}
                   </div>
                 </>
               )}
