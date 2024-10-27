03a1591abff8666b6efe4f2531b346fb88203320
baladevvibes <balamurugandevvibes@gmail.com>
2024-10-27 10:01:10 +0530

    updates

--- app/contact/contact-us-page-design/ContactUsPageDesign.js
+++ app/contact/contact-us-page-design/ContactUsPageDesign.js
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import "../../style.css";
 import Editor, { loader } from "@monaco-editor/react";
 import Footer from "../../components/Footer";
+import contactData from "../contactData";
 import testimonialsData from "../../testimonials/testimonialsData";
 import HomeComponents from "../../HomePage/HomeComponents";
 import componentConfig from "../../config/componentConfig";
@@ -45,7 +46,7 @@ export default function ContactUsPageDesign() {
       _id: 0,
       name: "app.js",
       language: "javascript",
-      value: testimonialsData[2]?.htmlcode,
+      value: contactData[2]?.htmlcode,
     },
     "index.css": {
       _id: 1,
--- app/contact/contactData.js
+++ app/contact/contactData.js
@@ -222,5 +222,98 @@ const contactData = [
         </section>
       </div>`,
   },
+  {
+    _id: 2,
+    name: "Contact us page design tailwind css",
+    title: "Contact us page design tailwind css",
+    htmlcode: `  <div class="py-20">
+        <div class="container mx-auto">
+          <div class="grid lg:grid-cols-2 lge:grid-cols-2 md:grid-cols-1  mdsm:grid-cols-1 sm:grid-cols-1">
+            <div class=" p-10 sm:p-2">
+              <h3 class="text-[#6256CA] font-semibold Kanit text-4xl">Contact us</h3>
+              <p class=" text-1xl REM-font text-[#4f4f4f] pb-3  pt-2">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries{" "}
+              </p>
+              <div class="">
+                <div class="pt-6">
+                  <div class="flex">
+                    <div class="h-[60px] flex   justify-center items-center bg-[#6256CA] w-[60px] rounded-lg">
+                    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class=" text-[#fff] text-2xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
+                    </div>
+                    <div class="mx-4">
+                      <div class=" pt-2">
+                        <p class=" Kanit text-[#4f4f4f] text-[16px]">
+                          Phone Number
+                        </p>
+                      </div>
+                      <div class="">
+                        <p class="text-[16px] REM-font  text-[#4f4f4f] ">0978456123</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="flex pt-6">
+                    <div class="h-[60px] flex   justify-center items-center bg-[#6256CA] w-[60px] rounded-lg">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class=" text-[#fff] text-2xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 1.95c-5.52 0-10 4.48-10 10s4.48 10 10 10h5v-2h-5c-4.34 0-8-3.66-8-8s3.66-8 8-8 8 3.66 8 8v1.43c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57v-1.43c0-2.76-2.24-5-5-5s-5 2.24-5 5 2.24 5 5 5c1.38 0 2.64-.56 3.54-1.47.65.89 1.77 1.47 2.96 1.47 1.97 0 3.5-1.6 3.5-3.57v-1.43c0-5.52-4.48-10-10-10zm0 13c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path></svg>
+                    </div>
+                    <div class="mx-4">
+                      <div class=" pt-2">
+                        <p class=" Kanit text-[#4f4f4f] text-[16px]">Email</p>
+                      </div>
+                      <div class="">
+                        <p class="text-[16px]  REM-font  text-[#4f4f4f] ">dummymailid@gmail.com</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="flex pt-6">
+                    <div class="h-[60px] flex   justify-center items-center bg-[#6256CA] w-[60px] rounded-lg">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class=" text-[#fff] text-2xl" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M515.664-.368C305.76-.368 128 178.4 128 390.176c0 221.76 206.032 448.544 344.624 607.936.528.64 22.929 25.52 50.528 25.52h2.449c27.6 0 49.84-24.88 50.399-25.52 130.064-149.52 320-396.048 320-607.936C896 178.4 757.344-.368 515.664-.368zm12.832 955.552c-1.12 1.12-2.753 2.369-4.193 3.409-1.472-1.008-3.072-2.288-4.255-3.408l-16.737-19.248C371.92 785.2 192 578.785 192 390.176c0-177.008 148.224-326.56 323.664-326.56 218.528 0 316.336 164 316.336 326.56 0 143.184-102.128 333.296-303.504 565.008zm-15.377-761.776c-106.032 0-192 85.968-192 192s85.968 192 192 192 192-85.968 192-192-85.968-192-192-192zm0 320c-70.576 0-129.473-58.816-129.473-129.408 0-70.576 57.424-128 128-128 70.624 0 128 57.424 128 128 .032 70.592-55.903 129.408-126.527 129.408z"></path></svg>
+                    </div>
+                    <div class="mx-4">
+                      <div class=" pt-2">
+                        <p class=" Kanit text-[#4f4f4f] text-[16px]">Address</p>
+                      </div>
+                      <div class="">
+                        <p class="text-[16px] REM-font  text-[#4f4f4f] ">
+                          56 High Street London W1B 2EL,<br/> United Kingdom
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="p-10 sm:p-2 ">
+              <div class="p-12 bg-[#edecf9] rounded-xl">
+                <form >
+                  <div>
+                  <label class="text-[#4f4f4f] font-medium REM-font ">Name</label>
+                  <input type="text" placeholder="Name" class="REM-font  text-[#4f4f4f] rounded-md mt-2  outline-1 outline-[#6054c9] border border-[#c2c2c2] py-2 w-full px-2"/>
+                  </div>
+                  <div class="pt-3">
+                  <label class="text-[#4f4f4f] font-medium ">Email</label>
+                  <input type="text" placeholder="email" class="REM-font  text-[#4f4f4f] rounded-md mt-2  outline-1 outline-[#6054c9] border border-[#c2c2c2] py-2 w-full px-2"/>
+                  </div>
+                  <div class="pt-3">
+                  <label class="text-[#4f4f4f]  font-medium ">Message</label>
+                  <textarea type="text" placeholder="Message" class="REM-font  text-[#4f4f4f] rounded-md mt-2  outline-1 outline-[#6054c9] border border-[#c2c2c2] py-2 w-full px-2"></textarea>
+                  </div>
+
+                  <div class=" w-full flex pt-4 mt-4 pb-0 justify-center ">
+                    <button  onClick={(e) => {
+      e.preventDefault();
+      window.location.href='http://makecomponents.com';
+      }} class="REM-font  bg-[#6256CA] hover:bg-[#222]  delay-200 transition-all rounded-full text-[#fff]  px-10 py-2">Submit </button>
+                  </div>
+                
+                  </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`,
+  },
 ];
 export default contactData;
